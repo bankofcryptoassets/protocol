9ddb4115d8dc80572a67259e30fc162427985916
Shreyas Padmakiran <81912496+gitshreevatsa@users.noreply.github.com>
2025-06-14 07:56:41 +0000

    contract version bump

--- constants.js
+++ constants.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const provider = new ethers.JsonRpcProvider(
   "https://sepolia.base.org", // change RPC URL
 );
-const contractAddress = "0x920D7263014303530E92eE98E4eb4599a0a8af0E"; // change
+const contractAddress = "0x8634d56D28dA9925ECd16FE4c22f044E5e3eB59c"; // change
 const USDC_Address = "0x377faBD7d29562c059Dd2D3A9C41eF6974d26B21";
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
