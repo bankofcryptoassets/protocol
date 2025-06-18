82d9f0bbd661830a9b2d95a4c1dc37e1d79b466e
gitshreevatsa <vastas2002@gmail.com>
2025-06-18 13:02:20 +0530

    add new contract

--- constants.js
+++ constants.js
@@ -6,7 +6,7 @@ const provider = new ethers.JsonRpcProvider(
   "https://sepolia.base.org", // change RPC URL
 );
 
-const contractAddress = "0x5eAe025301215fCda0cb4432FDA51D6580fCc763"; // change
+const contractAddress = "0x105Ba0E6d0111d80C82eFE8AF38998CddC5aA96A"; // change
 const USDC_Address = "0x377faBD7d29562c059Dd2D3A9C41eF6974d26B21";
 const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
 
